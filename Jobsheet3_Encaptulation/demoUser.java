0bbbed6877c58a31462a32b128af13392fd88995
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-09-20 14:31:06 +0700

    Praticum 3 - JS 03

---
+++ Jobsheet3_Encaptulation/demoUser.java
@@ -0,0 +1,8 @@
+package Jobsheet3;
+
+public class demoUser {
+    public static void main(String[] args) {
+        user user2 = new user("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
+        user2.displayInfo();
+    }
+}
