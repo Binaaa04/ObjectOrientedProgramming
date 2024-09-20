e995dfdd4e058dd76efdb58ccdb13f2130141a65
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-09-20 14:29:59 +0700

    Delete Jobsheet3_Encaptulation/demoUser.java

--- Jobsheet3_Encaptulation/demoUser.java
+++
@@ -1,8 +0,0 @@
-package Jobsheet3;
-
-public class demoUser {
-    public static void main(String[] args) {
-        user user2 = new user("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
-        user2.displayInfo();
-    }
-}
