83497da694a9b3a3a82682435afd79dca6b26e23
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-02 19:05:16 +0700

    JS6_Practicum 3_Access rights (Modify 2)

--- Jobsheet6_Inheritance/employee.java
+++ Jobsheet6_Inheritance/employee.java
@@ -1,14 +1,25 @@
+package testpackage;
+
 /**
  * employee
  */
 public class employee {
     public String nip;
     public String name;
-    private double salary;
+    protected double salary;
 
     public employee() {
         System.out.println("Object from employee");
     }
+
+    public String getlnfo() {
+        String info = "";
+        info += "NIP        : " + nip + "\n";
+        info += "Name       : " + name + "\n";
+        info += "Salary     : " + salary + "\n";
+        return info;
+    }
+}
 
     public String getlnfo() {
         String info = "";
