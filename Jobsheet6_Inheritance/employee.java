916b0cb60296738d199dbab3842b920508392f42
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-02 10:40:09 +0700

    JS6_Praticum 2_Inheritance

--- Jobsheet6_Inheritance/employee.java
+++ Jobsheet6_Inheritance/employee.java
@@ -2,7 +2,19 @@
  * employee
  */
 public class employee {
+    public String nip;
+    public String name;
+    public double salary;
+
     public employee() {
         System.out.println("Object from employee");
+    }
+
+    public String getlnfo() {
+        String info = "";
+        info += "NIP        : " + nip + "\n";
+        info += "Name       : " + name + "\n";
+        info += "Salary     : " + salary + "\n";
+        return info;
     }
 }
