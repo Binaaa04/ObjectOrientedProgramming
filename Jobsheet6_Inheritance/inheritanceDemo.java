8c629f481c8f0815e7003803952199faa3a59186
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-02 20:41:50 +0700

    JS6_Practicum 6_Overloading

--- Jobsheet6_Inheritance/inheritanceDemo.java
+++ Jobsheet6_Inheritance/inheritanceDemo.java
@@ -5,6 +5,10 @@ public class inheritanceDemo {
         lecture1.nip = "34329837";
         lecture1.salary = 3000000;
         lecture1.nidn = "1989432439";
-        System.out.println(lecture1.getlnfo());
+        System.out.println(lecture1.getAllInfo());
+
+        lecture lecture2 = new lecture("34329837", "Yansy Ayuningtyas", 3000000, "1989432439");
+        System.out.println(lecture2.getAllInfo());
+
     }
 }
