821243fd9e661277be246f997be030cc2a2f4cd1
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-02 19:18:08 +0700

    JS6_Practicum 4_Super - attributes

--- Jobsheet6_Inheritance/lecture.java
+++ Jobsheet6_Inheritance/lecture.java
@@ -1,9 +1,16 @@
-import testpackage.employee;
-
 public class lecture extends employee {
     public String nidn;
 
     public lecture() {
         System.out.println("Object from lecture class");
+    }
+
+    public String getAllInfo() {
+        String info = "";
+        info += "NIP        : " + nip + "\n";
+        info += "NIDN       : " + nidn + "\n";
+        info += "Name       : " + name + "\n";
+        info += "Salary     : " + salary + "\n";
+        return info;
     }
 }
