9ac911fc607e3129f0d1b3c39fc318bbe890fdbc
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-02 20:41:03 +0700

    JS6_Practicum 6_Overloading

--- Jobsheet6_Inheritance/lecture.java
+++ Jobsheet6_Inheritance/lecture.java
@@ -5,6 +5,10 @@ public class lecture extends employee {
         System.out.println("Object from lecture class");
     }
 
+    public lecture(String nip, String name, double salary, String nidn) {
+        System.out.println("objects of the lecturer class are created with parameterized constructors");
+    }
+
     public String getAllInfo() {
         String info = super.getlnfo();
         info += "NIDN       : " + nidn + "\n";
