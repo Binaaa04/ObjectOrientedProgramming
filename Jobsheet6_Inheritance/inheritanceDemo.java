561d5bc29167d6b97aa2279699eee7346173a673
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-02 14:58:27 +0700

    JS6_Practicum 2_Inheritance

--- Jobsheet6_Inheritance/inheritanceDemo.java
+++ Jobsheet6_Inheritance/inheritanceDemo.java
@@ -1,5 +1,10 @@
 public class inheritanceDemo {
     public static void main(String[] args) {
         lecture lecture1 = new lecture();
+        lecture1.name = "Yansy Ayuningtyas";
+        lecture1.nip = "34329837";
+        lecture1.salary = 3000000;
+        lecture1.nidn = "1989432439";
+        System.out.println(lecture1.getlnfo());
     }
 }
