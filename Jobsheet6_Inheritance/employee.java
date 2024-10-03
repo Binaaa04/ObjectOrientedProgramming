24c8de17a3099dd1ff7dac83a93613b8bcb0ed90
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-03 12:21:01 +0700

    JS6_Practicum 7_ Super constructor

--- Jobsheet6_Inheritance/employee.java
+++ Jobsheet6_Inheritance/employee.java
@@ -1,25 +1,20 @@
-package testpackage;
-
 /**
  * employee
  */
 public class employee {
     public String nip;
     public String name;
-    protected double salary;
+    public double salary;
 
     public employee() {
-        System.out.println("Object from employee");
+        // System.out.println("Object from employee");
     }
 
-    public String getlnfo() {
-        String info = "";
-        info += "NIP        : " + nip + "\n";
-        info += "Name       : " + name + "\n";
-        info += "Salary     : " + salary + "\n";
-        return info;
+    public employee(String nip, String name, double salary) {
+        this.nip = nip;
+        this.name = name;
+        this.salary = salary;
     }
-}
 
     public String getlnfo() {
         String info = "";
