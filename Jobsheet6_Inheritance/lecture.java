974d8d1dc523a81a5cbf45039e0f7a2174cff879
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-03 12:27:36 +0700

    JS6_Practicum 7_ Super constructor

--- Jobsheet6_Inheritance/lecture.java
+++ Jobsheet6_Inheritance/lecture.java
@@ -2,16 +2,12 @@ public class lecture extends employee {
     public String nidn;
 
     public lecture() {
-        System.out.println("Object from lecture class");
+        // System.out.println("Object from lecture class");
     }
 
     public lecture(String nip, String name, double salary, String nidn) {
-        super();
-        super.nip = nip;
-        super.name = name;
-        super.salary = salary;
         this.nidn = nidn;
-        System.out.println("objects of the lecturer class are created with parameterized constructors");
+        super(nip, name, salary);
     }
 
     public String getAllInfo() {
