2da2ae3e120458757ee1b5e2f47c549bcf0fd53c
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-04 09:19:19 +0700

    a

---
+++ Jobsheet6_Inheritance/makeupDemo.java
@@ -0,0 +1,15 @@
+public class makeupDemo {
+    public static void main(String[] args) {
+        cushion cushionDesc = new cushion();
+        cushionDesc.shade = "Warm Beige";
+        cushionDesc.brand = "Mother of Pearl";
+        cushionDesc.price = 209.000;
+        cushionDesc.type = "Cushion";
+        cushionDesc.setProductID("44540001");
+        System.out.println(cushionDesc.getAllInfo());
+
+        liptint liptintDesc = new liptint("PK02", "Makeover", "Liptint", 169.000);
+        liptintDesc.setProductID("43252493");
+        System.out.println(liptintDesc.getAllInfo());
+    }
+}
